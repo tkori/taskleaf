078f48d4fe46aecb60b38dc23bff59da07e3ffd3
tkori <tkori19+github@gmail.com>
2019-09-28 14:57:36 +0900

    Add the validation without comma to the task name

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_commma
+
+  private
+
+  def validate_name_not_including_commma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
